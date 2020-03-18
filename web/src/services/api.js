e747aac879391941805b01416c3f5f58dc9d257e
Michelon Souza <michelon.souza@hotmail.com>
2020-03-18 01:58:39 -0300

    fix: upgrade dependencies in web, mobile and backend

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,33 @@
 import axios from 'axios';
+import history from './history';
+
+import { store } from '~/store';
+import { logOut } from '~/store/modules/auth/actions';
+
 
 const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    // eslint-disable-next-line no-console
+    console.log('status:error', [error]);
+
+    if (
+      error.response.status > 400 &&
+      error.response.status < 404 &&
+      window.location.pathname !== '/'
+    ) {
+      store.dispatch(logOut());
+      history.push('/');
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
